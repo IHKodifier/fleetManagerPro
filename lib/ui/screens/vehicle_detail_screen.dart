f2f792e1db1cdbb1e6058a5bed448860cb740a62
IHKodifier <51688451+IHKodifier@users.noreply.github.com>
2023-04-03 08:48:29 +0500

    vehicle image loading _ circularprogressIndicator

--- lib/ui/screens/vehicle_detail_screen.dart
+++ lib/ui/screens/vehicle_detail_screen.dart
@@ -11,131 +11,120 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as Path;
 
-
-
-
-
-
-
-
-
 class VehicleDetailScreen extends ConsumerStatefulWidget {
   const VehicleDetailScreen({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _VehicleDetailScreenState();
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _VehicleDetailScreenState();
 }
 
 class _VehicleDetailScreenState extends ConsumerState<VehicleDetailScreen> {
-    late Vehicle state = Vehicle(id: '');
+  final pageController = PageController(initialPage: 1);
+  late Vehicle state = Vehicle(id: '');
 
   late BuildContext _context;
 
-  void onFABPressed() {
-  }
+  void onFABPressed() {}
 
-   body(BuildContext context) {
+  body(BuildContext context) {
     _context = context;
     return SingleChildScrollView(
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          SizedBox(
-            height: 320,
-            child: Card(
-              margin: EdgeInsets.all(16),
-              child: Stack(children: [
-                AspectRatio(
-                  aspectRatio: 1.618,
-                  child: state.images?.length!=0?
-                  Image.network(state.images![0]! ):
-                  Image.asset('assets/honda.jpg'),
-                ),
-                Positioned(
-                  top: 10,
-                  left: 8,
-                  child: Text(
-                    state.make!,
-                    style: Theme.of(context).textTheme.headlineLarge,
+          Card(
+            margin: EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 350,
+                  child: Stack(
+                    children: [
+                      PageView(
+                        controller: pageController,
+                        children: state.images!
+                            .map(
+                              (e) => AspectRatio(
+                                aspectRatio: 1.618,
+                                child: Image.network(e!,
+                                loadingBuilder:(context,widget,data )=>Center(child: CircularProgressIndicator(),
+                                ),
+                                frameBuilder: (context, child, frame, wasSynchronouslyLoaded) => child,),
+                              ),
+                            )
+                            .toList(),
+                      ),
+                    ],
                   ),
                 ),
-                Positioned(
-                  bottom: 10,
-                  left: 8,
-                  child: Padding(
-                    padding: const EdgeInsets.all(4.0),
-                    child: Text(
-                      state.model!,
-                      style: Theme.of(context).textTheme.headlineSmall,
+              ],
+            ),
+          ),
+          Positioned(
+            top: 10,
+            left: 8,
+            child: Text(
+              state.make!,
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+          ),
+          PositionedModelWidget(state: state),
+          PositionedYearWidget(state: state),
+          Positioned(
+            bottom: 10,
+            right: 8,
+            left: 8,
+            child: IconButton(
+              icon: Icon(Icons.add_a_photo),
+              onPressed: () {
+                showDialog(
+                  context: context,
+                  builder: (context) => Dialog(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: () async {
+                                  _pickImage(imageSource: ImageSource.camera);
+                                },
+                                icon: Icon(
+                                  Icons.camera,
+                                  size: 45,
+                                ),
+                              ),
+                              Text('Take Photo'),
+                            ],
+                          ),
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                onPressed: () async {
+                                  _pickImage(imageSource: ImageSource.gallery);
+                                },
+                                icon: Icon(
+                                  Icons.photo_album,
+                                  size: 45,
+                                ),
+                              ),
+                              Text('Choose from Gallery'),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Positioned(
-                  bottom: 10,
-                  right: 8,
-                  child: Text(
-                    state.year!,
-                    style: Theme.of(context).textTheme.headlineSmall,
-                  ),
-                ),
-                Positioned(
-                  bottom: 10,
-                  right: 8,
-                  left: 8,
-                  child: IconButton(
-                    icon: Icon(Icons.add_a_photo),
-                    onPressed: () {
-                      showDialog(
-                        context: context,
-                        builder: (context) => Dialog(
-                          child: Padding(
-                            padding: const EdgeInsets.all(16.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    IconButton(
-                                      onPressed: () async {
-                                        _pickImage(
-                                            imageSource: ImageSource.camera);
-                                      },
-                                      icon: Icon(
-                                        Icons.camera,
-                                        size: 45,
-                                      ),
-                                    ),
-                                    Text('Take Photo'),
-                                  ],
-                                ),
-                                Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    IconButton(
-                                      onPressed: () async {
-                                        _pickImage(
-                                            imageSource: ImageSource.camera);
-                                      },
-                                      icon: Icon(
-                                        Icons.photo_album,
-                                        size: 45,
-                                      ),
-                                    ),
-                                    Text('Choose from Gallery'),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ]),
+                );
+              },
             ),
           ),
         ],
@@ -143,7 +132,7 @@ class _VehicleDetailScreenState extends ConsumerState<VehicleDetailScreen> {
     );
   }
 
-    void _pickImage({required ImageSource imageSource}) async {
+  void _pickImage({required ImageSource imageSource}) async {
     final pickedFile = await ImagePicker().pickImage(source: imageSource);
     if (pickedFile != null) {
       final file = File(pickedFile.path);
@@ -151,28 +140,33 @@ class _VehicleDetailScreenState extends ConsumerState<VehicleDetailScreen> {
     }
   }
 
-void _uploadImage(File file) async {
-  final appuser = ref.read(appUserProvider);
-  final vehicle = ref.read(currentVehicleProvider);
-  final fileName = Path.basename(file.path);
-  final Reference firebaseStorageRef =
-      FirebaseStorage.instance.ref().child('userdata/${appuser?.uuid}/images/$fileName');
-  final UploadTask uploadTask = firebaseStorageRef.putFile(file);
-  await uploadTask.whenComplete(() async {
-    final String downloadURL = await firebaseStorageRef.getDownloadURL();
-    print('File uploaded to Firebase at $downloadURL');
-    final DocumentReference docRef = FirebaseFirestore.instance.collection('users').doc(appuser?.uuid).collection('vehicles').doc(vehicle.id);
-    final List<String> imagesList = List<String>.from(state.images ?? []);
-    imagesList.add(downloadURL);
-    await docRef.update({'images': imagesList});
-   ref.refresh(currentVehicleProvider);
-    Navigator.pop(context);
-  });
-}
+  void _uploadImage(File file) async {
+    final appuser = ref.read(appUserProvider);
+    final vehicle = ref.read(currentVehicleProvider);
+    final fileName = Path.basename(file.path);
+    final Reference firebaseStorageRef = FirebaseStorage.instance
+        .ref()
+        .child('userdata/${appuser?.uuid}/images/$fileName');
+    final UploadTask uploadTask = firebaseStorageRef.putFile(file);
+    await uploadTask.whenComplete(() async {
+      final String downloadURL = await firebaseStorageRef.getDownloadURL();
+      print('File uploaded to Firebase at $downloadURL');
+      final DocumentReference docRef = FirebaseFirestore.instance
+          .collection('users')
+          .doc(appuser?.uuid)
+          .collection('vehicles')
+          .doc(vehicle.id);
+      final List<String> imagesList = List<String>.from(state.images ?? []);
+      imagesList.add(downloadURL);
+      await docRef.update({'images': imagesList});
+      ref.refresh(currentVehicleProvider);
+      Navigator.pop(context);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-      state = ref.watch(currentVehicleProvider);
+    state = ref.watch(currentVehicleProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -187,6 +181,51 @@ void _uploadImage(File file) async {
             size: 40,
           ),
           tooltip: 'Add Maintenance'),
+    );
+  }
+}
+
+class PositionedModelWidget extends StatelessWidget {
+  const PositionedModelWidget({
+    super.key,
+    required this.state,
+  });
+
+  final Vehicle state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 12,
+      left: 8,
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Text(
+          state.model!,
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+      ),
+    );
+  }
+}
+
+class PositionedYearWidget extends StatelessWidget {
+  const PositionedYearWidget({
+    super.key,
+    required this.state,
+  });
+
+  final Vehicle state;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      bottom: 10,
+      right: 8,
+      child: Text(
+        state.year!,
+        style: Theme.of(context).textTheme.headlineSmall,
+      ),
     );
   }
 }
