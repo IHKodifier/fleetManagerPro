3ae97bf6119ff16b22e34d12b0f520b8ee04f3b9
IHK <ihk@email.com>
2023-06-19 20:27:13 -0700

    updating maintenance card

--- lib/ui/shared/maintenance_card.dart
+++ lib/ui/shared/maintenance_card.dart
@@ -24,53 +24,16 @@ class MaintenanceCard extends StatelessWidget {
         // elevation:15,
         margin: EdgeInsets.all(8),
         elevation: 15,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.start,
-          mainAxisSize: MainAxisSize.min,
+        child: Stack(
+          // crossAxisAlignment: CrossAxisAlignment.start,
+          // mainAxisAlignment: MainAxisAlignment.start,
+          // mainAxisSize: MainAxisSize.min,
           children: [
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8),
               child: Row(
-                mainAxisSize: MainAxisSize.min,
                 children: [
-                 Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.min,
-                   children: [
-                     Container(
-                      padding: EdgeInsets.all(4),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(18),
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                          Theme.of(context).colorScheme.secondary,
-                          Theme.of(context).colorScheme.onPrimary,
-                          
-                        ]),
-                        border: Border.all(
-                          color:  Theme.of(context).colorScheme.onSecondary,
-                          width: 0.75,
-                        ),
-                      ),
-                  
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            // Spacer(),
-                            Text(Utils.thousandify(totalDriven-maintenance.kmsDriven),style: Theme.of(context).textTheme.headlineSmall),
-                             Text('Kms ago',style: TextStyle(color: Colors.black,fontSize: 12),),
-                            // Spacer(),
-                          ],
-                        ),
-                      )),
-                     
-                   ],
-                 ),
+                 buildLeadingWidget(context),
                 //  Expanded(child: Container(color:Colors.red)),
                   Container(
                     child: Expanded(child: Column(
@@ -79,7 +42,7 @@ class MaintenanceCard extends StatelessWidget {
                       mainAxisSize: MainAxisSize.min,
                       children: [
                         Padding(
-                          padding: const EdgeInsets.fromLTRB(4,0,4,0),
+                          padding: const EdgeInsets.fromLTRB(4,0,0,0),
                           child: Text(maintenance.location!),
                         ),
             
@@ -96,18 +59,68 @@ class MaintenanceCard extends StatelessWidget {
                 ],
               ),
             ),
-            // ...maintenance.services!.map((e) => Row(
-            //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            //   children: [
-            //     Text(e!.name),
-            //     Text(e.cost.toString()),
-            //   ],
-            // ))
+            ...maintenance.services!.map((e) => Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(e!.name),
+                SizedBox(width: 150),
+                Text(e.cost.toString()),
+              ],
+            ))
           ],
-    
+          
           // Add more details about the maintenance object as needed
         ),
       ),
     );
+  }
+
+  Column buildLeadingWidget(BuildContext context) {
+    return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.min,
+                 children: [
+                   buildLeadingText(context),
+                   
+                 ],
+               );
+  }
+
+  Container buildLeadingText(BuildContext context) {
+    return Container(
+                      padding: EdgeInsets.all(4),
+                      decoration: buildLeadingDecoration(context),
+                                     
+                      child: Padding(
+                        padding: const EdgeInsets.all(4.0),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          mainAxisSize: MainAxisSize.max,
+                          children: [
+                            // Spacer(),
+                            Text(Utils.thousandify(totalDriven-maintenance.kmsDriven),style: Theme.of(context).textTheme.headlineSmall),
+                             Text('Kms ago',style: TextStyle(color: Colors.black,fontSize: 12),),
+                            // Spacer(),
+                          ],
+                        ),
+                      ));
+  }
+
+  BoxDecoration buildLeadingDecoration(BuildContext context) {
+    return BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                        Theme.of(context).colorScheme.secondary,
+                        Theme.of(context).colorScheme.onPrimary,
+                        
+                      ]),
+                      border: Border.all(
+                        color:  Theme.of(context).colorScheme.onSecondary,
+                        width: 0.75,
+                      ),
+                    );
   }
 }
