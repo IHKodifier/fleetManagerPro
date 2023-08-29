20f5cd53e1dda7ab427372703cfd1d5bd285434c
IHK_Office_Laptop <IHK_Office_Laptop>
2023-08-29 10:46:24 +0500

    added FuelStop and Logbook model classes

---
+++ lib/states/fuelstop.dart
@@ -0,0 +1,63 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+
+class FuelStop extends Equatable {
+  final String id;
+  final int driven;
+  final double litres;
+  double pricePerLitre;
+  final double totalCost;
+  final Timestamp timestamp;
+
+  FuelStop._({
+    required this.id,
+    required this.driven,
+    required this.litres,
+    required this.pricePerLitre,
+    required this.totalCost,
+    required this.timestamp,
+  });
+
+  factory FuelStop({
+    required String id,
+    required int driven,
+    required double litres,
+    double? pricePerLitre,
+    required double totalCost,
+    Timestamp? timestamp,
+  }) {
+    return FuelStop._(
+      id: id,
+      driven: driven,
+      litres: litres,
+      pricePerLitre: pricePerLitre ?? totalCost / litres,
+      totalCost: totalCost,
+      timestamp: timestamp ?? Timestamp.now(),
+    );
+  }
+
+  @override
+  List<Object> get props =>
+      [id, driven, litres, pricePerLitre, totalCost, timestamp];
+
+  @override
+  bool get stringify => true;
+
+  FuelStop copyWith({
+    String? id,
+    int? driven,
+    double? litres,
+    double? pricePerLitre,
+    double? totalCost,
+    Timestamp? timestamp,
+  }) {
+    return FuelStop._(
+      id: id ?? this.id,
+      driven: driven ?? this.driven,
+      litres: litres ?? this.litres,
+      pricePerLitre: pricePerLitre ?? this.pricePerLitre,
+      totalCost: totalCost ?? this.totalCost,
+      timestamp: timestamp ?? this.timestamp,
+    );
+  }
+}
---
+++ lib/states/logbook.dart
@@ -0,0 +1,76 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:equatable/equatable.dart';
+
+class Logbook extends Equatable {
+  final String id;
+  final Timestamp timestamp;
+  final List<String> placesVisited;
+  final double kmsTravelled;
+  final int startReading;
+  final int endReading;
+  final String driver;
+
+  Logbook._({
+    required this.id,
+    required this.timestamp,
+    required this.placesVisited,
+    required this.kmsTravelled,
+    required this.startReading,
+    required this.endReading,
+    required this.driver,
+  });
+
+  factory Logbook({
+    required String id,
+    Timestamp? timestamp,
+    List<String>? placesVisited,
+    required double kmsTravelled,
+    required int startReading,
+    required int endReading,
+    required String driver,
+  }) {
+    return Logbook._(
+      id: id,
+      timestamp: timestamp ?? Timestamp.now(),
+      placesVisited: placesVisited ?? [],
+      kmsTravelled: kmsTravelled,
+      startReading: startReading,
+      endReading: endReading,
+      driver: driver,
+    );
+  }
+
+  @override
+  List<Object> get props => [
+        id,
+        timestamp,
+        placesVisited,
+        kmsTravelled,
+        startReading,
+        endReading,
+        driver
+      ];
+
+  @override
+  bool get stringify => true;
+
+  Logbook copyWith({
+    String? id,
+    Timestamp? timestamp,
+    List<String>? placesVisited,
+    double? kmsTravelled,
+    int? startReading,
+    int? endReading,
+    String? driver,
+  }) {
+    return Logbook._(
+      id: id ?? this.id,
+      timestamp: timestamp ?? this.timestamp,
+      placesVisited: placesVisited ?? this.placesVisited,
+      kmsTravelled: kmsTravelled ?? this.kmsTravelled,
+      startReading: startReading ?? this.startReading,
+      endReading: endReading ?? this.endReading,
+      driver: driver ?? this.driver,
+    );
+  }
+}
